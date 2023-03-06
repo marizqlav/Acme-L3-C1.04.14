090cf19b73c52409192e2d0c46e8ed9c4c54254d
ismherram <ismherram@alum.us.es>
2023-03-06 19:00:57 +0100

    Add Peep entity

--- src/main/java/acme/features/authenticated/student/AuthenticatedStudentRepository.java
+++ src/main/java/acme/features/authenticated/student/AuthenticatedStudentRepository.java
@@ -25,7 +25,7 @@ public interface AuthenticatedStudentRepository extends AbstractRepository {
 	@Query("select ua from UserAccount ua where ua.id = :id")
 	UserAccount findOneUserAccountById(int id);
 
-	@Query("select c from Student c where c.userAccount.id = :id")
+	@Query("select s from Student s where s.userAccount.id = :id")
 	Student findOneStudentByUserAccountId(int id);
 
 }
