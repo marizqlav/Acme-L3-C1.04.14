ab2a53bb4c43bcdfa568f9c3d5a044c469a01330
alemerpal <alemerpal@alum.us.es>
2023-03-09 16:41:34 +0100

    banner

--- src/main/java/acme/audits/AuditingRecords.java
+++ src/main/java/acme/audits/AuditingRecords.java
@@ -5,7 +5,6 @@ package acme.audits;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
 import acme.framework.data.AbstractEntity;
