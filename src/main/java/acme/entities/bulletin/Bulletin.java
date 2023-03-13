f82e99c5fa219b8641d00e1cf18ec2c72ec23eec
Marcos <marolmmar1@alum.us.es>
2023-03-13 11:00:52 +0100

    mergeado con main

---
+++ src/main/java/acme/entities/bulletin/Bulletin.java
@@ -0,0 +1,52 @@
+
+package acme.entities.bulletin;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Bulletin extends AbstractEntity {
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1L;
+
+	@Column(name = "title")
+	@NotBlank
+	@Size(max = 76)
+	protected String			title;
+
+	@Column(name = "date")
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	@PastOrPresent
+	protected Date				date;
+
+	@Column(name = "message")
+	@NotBlank
+	@Size(max = 101)
+	protected String			message;
+
+	// Critical is True if the bulletin is critical, false otherwise
+	@Column(name = "critical")
+	@NotNull
+	protected Boolean			critical;
+
+	@Column(name = "link")
+	protected String			link;
+}
