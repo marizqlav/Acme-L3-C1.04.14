15d48b635a36c01b6516191b416e1c190235c24f
Mario <marizqlav@alum.us.es>
2023-05-15 17:36:45 +0200

    Test de sesionPracticum

--- src/test/java/acme/testing/company/sessionPracticum/CompanySessionPracticumPublishTest.java
+++ src/test/java/acme/testing/company/sessionPracticum/CompanySessionPracticumPublishTest.java
@@ -1,6 +1,7 @@
 
 package acme.testing.company.sessionPracticum;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,13 +37,38 @@ public class CompanySessionPracticumPublishTest extends TestHarness {
 	//
 	//		super.signOut();
 	//	}
+	//
+	//	@ParameterizedTest
+	//	@CsvFileSource(resources = "/company/session-practicum/publish-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	//	public void negativeButtonTest(final int recordIndex, final int sesionRecordIndex, final String title, final String abstractSessionPracticum) {
+	//		// HINT: this test attempts to publish a Practicum that cannot be published, yet.
+	//
+	//		super.signIn("company1", "company1");
+	//
+	//		super.clickOnMenu("Company", "Practicum list");
+	//		super.checkListingExists();
+	//		super.sortListing(0, "asc");
+	//
+	//		super.clickOnListingRecord(recordIndex);
+	//		super.checkFormExists();
+	//		super.clickOnButton("List Session Practicum");
+	//
+	//		super.sortListing(0, "asc");
+	//		super.checkColumnHasValue(sesionRecordIndex, 0, title);
+	//		super.checkColumnHasValue(sesionRecordIndex, 1, abstractSessionPracticum);
+	//
+	//		super.clickOnListingRecord(recordIndex);
+	//		super.checkFormExists();
+	//		super.checkNotSubmitExists("Publish");
+	//
+	//		super.signOut();
+	//	}
 
 
 	@ParameterizedTest
-	@CsvFileSource(resources = "/company/session-practicum/publish-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void negativeButtonTest(final int recordIndex, final int sesionRecordIndex, final String title, final String abstractSessionPracticum) {
-		// HINT: this test attempts to publish a Practicum that cannot be published, yet.
-
+	@CsvFileSource(resources = "/company/session-practicum/publish-negative2.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void negativeTest(final int recordIndex, final int sesionRecordIndex, final String title, final String abstractSessionPracticum, final String startDate, final String finishDate, final String link) {
 		super.signIn("company1", "company1");
 
 		super.clickOnMenu("Company", "Practicum list");
@@ -59,67 +85,26 @@ public class CompanySessionPracticumPublishTest extends TestHarness {
 
 		super.clickOnListingRecord(recordIndex);
 		super.checkFormExists();
-		super.checkNotSubmitExists("Publish");
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("abstractSessionPracticum", abstractSessionPracticum);
+		super.fillInputBoxIn("startDate", startDate);
+		super.fillInputBoxIn("finishDate", finishDate);
+		super.fillInputBoxIn("link", link);
+		super.clickOnSubmit("Publish");
 
+		super.checkErrorsExist();
 		super.signOut();
 	}
 
-	//		@Test
-	//		public void test300Hacking() {
-	//			// HINT: this test tries to publish a Practicum with a role other than "Employer".
-	//	
-	//			final Collection<Practicum> practicums;
-	//			String params;
-	//	
-	//			practicums = this.repository.findManyPracticumsByCompanyUsername("company1");
-	//			for (final Practicum practicum : practicums)
-	//				if (practicum.getDraftMode()) {
-	//					params = String.format("id=%d", practicum.getId());
-	//	
-	//					super.checkLinkExists("Sign in");
-	//					super.request("/company/practicum/publish", params);
-	//					super.checkPanicExists();
-	//	
-	//					super.signIn("administrator1", "administrator1");
-	//					super.request("/company/practicum/publish", params);
-	//					super.checkPanicExists();
-	//					super.signOut();
-	//	
-	//				}
-	//		}
-	//	
-	//		@Test
-	//		public void test301Hacking() {
-	//			// HINT: this test tries to publish a published Practicum that was registered by the principal.
-	//	
-	//			Collection<Practicum> practicums;
-	//			String params;
-	//	
-	//			super.signIn("company1", "company1");
-	//			practicums = this.repository.findManyPracticumsByCompanyUsername("company1");
-	//			for (final Practicum practicum : practicums)
-	//				if (!practicum.getDraftMode()) {
-	//					params = String.format("id=%d", practicum.getId());
-	//					super.request("/company/practicum/publish", params);
-	//				}
-	//			super.signOut();
-	//		}
-	//	
-	//		@Test
-	//		public void test302Hacking() {
-	//			// HINT: this test tries to publish a Practicum that wasn't registered by the principal,
-	//			// HINT+ be it published or unpublished.
-	//	
-	//			Collection<Practicum> practicums;
-	//			String params;
-	//	
-	//			super.signIn("company2", "company2");
-	//			practicums = this.repository.findManyPracticumsByCompanyUsername("company1");
-	//			for (final Practicum practicum : practicums) {
-	//				params = String.format("id=%d", practicum.getId());
-	//				super.request("/company/practicum/session-practicum/publish", params);
-	//			}
-	//			super.signOut();
-	//		}
+	//	@Test
+	//	public void hackingTest() {
+	//		super.request("/company/session-practicum/publish");
+	//		super.checkPanicExists();
+	//
+	//		super.signIn("company1", "company1");
+	//		super.request("/company/session-practicum/publish");
+	//		super.checkPanicExists();
+	//		super.signOut();
+	//	}
 
 }
