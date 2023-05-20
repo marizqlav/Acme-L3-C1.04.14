46166858d8438667859b5d70dead1440dddbfcc2
albdomrui <albdomrui@alum.us.es>
2023-05-20 13:04:01 +0200

    Test negativo de Lectures

--- src/test/java/acme/testing/lecturer/lecture/LecturerLectureCreateTest.java
+++ src/test/java/acme/testing/lecturer/lecture/LecturerLectureCreateTest.java
@@ -1,6 +1,7 @@
 
 package acme.testing.lecturer.lecture;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -40,38 +41,39 @@ public class LecturerLectureCreateTest extends TestHarness {
 		super.signOut();
 	}
 
-	//	@ParameterizedTest
-	//	@CsvFileSource(resources = "/lecturer/course/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-	//	public void negativeTest(final int recordIndex, final String title, final String resumen, final String retailPrice, final String link) {
-	//
-	//		super.signIn("lecturer1", "lecturer1");
-	//		super.clickOnMenu("Lecturer", "Course List");
-	//		super.clickOnButton("Create Course");
-	//		super.checkFormExists();
-	//
-	//		super.fillInputBoxIn("title", title);
-	//		super.fillInputBoxIn("resumen", resumen);
-	//		super.fillInputBoxIn("retailPrice", retailPrice);
-	//		super.fillInputBoxIn("link", link);
-	//		super.clickOnSubmit("Create Course");
-	//
-	//		super.checkErrorsExist();
-	//		super.signOut();
-	//	}
-	//
-	//	@Test
-	//	public void test300Hacking() {
-	//		// HINT: this test tries to create a job using principals with
-	//		// HINT+ inappropriate roles.
-	//
-	//		super.checkLinkExists("Sign in");
-	//		super.request("/lecturer/course/create");
-	//		super.checkPanicExists();
-	//
-	//		super.signIn("administrator1", "administrator1");
-	//		super.request("/lecturer/course/create");
-	//		super.checkPanicExists();
-	//		super.signOut();
-	//	}
+	@ParameterizedTest
+	@CsvFileSource(resources = "/lecturer/lecture/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void negativeTest(final int recordIndex, final String title, final String resumen, final String estimatedTime, final String body, final String lectureType) {
+
+		super.signIn("lecturer1", "lecturer1");
+		super.clickOnMenu("Lecturer", "Lecture List");
+		super.clickOnButton("Create Lecture");
+		super.checkFormExists();
+
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("resumen", resumen);
+		super.fillInputBoxIn("estimatedTime", estimatedTime);
+		super.fillInputBoxIn("body", body);
+		super.fillInputBoxIn("lectureType", lectureType);
+		super.clickOnSubmit("Create Lecture");
+
+		super.checkErrorsExist();
+		super.signOut();
+	}
+
+	@Test
+	public void test300Hacking() {
+		// HINT: this test tries to create a job using principals with
+		// HINT+ inappropriate roles.
+
+		super.checkLinkExists("Sign in");
+		super.request("/lecturer/leture/create");
+		super.checkPanicExists();
+
+		super.signIn("administrator1", "administrator1");
+		super.request("/lecturer/lecture/create");
+		super.checkPanicExists();
+		super.signOut();
+	}
 
 }
