5230eeb9afa7c266eaf60711e9bed8a7e376c51f
albdomrui <albdomrui@alum.us.es>
2023-05-21 22:17:32 +0200

    Delete course test finalizado

--- src/test/java/acme/testing/lecturer/course/LecturerCourseDeleteTest.java
+++ src/test/java/acme/testing/lecturer/course/LecturerCourseDeleteTest.java
@@ -1,33 +1,86 @@
 
 package acme.testing.lecturer.course;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
+import java.util.Collection;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.entities.courses.Course;
 import acme.testing.TestHarness;
 
 public class LecturerCourseDeleteTest extends TestHarness {
 
-	@ParameterizedTest
-	@CsvFileSource(resources = "/lecturer/course/delete-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void positiveTest(final int recordIndex, final String code, final String title, final String resumen, final String retailPrice, final String link) {
+	@Autowired
+	protected LecturerCourseTestRepository repository;
 
-		super.signIn("lecturer1", "lecturer1");
-		super.clickOnMenu("Lecturer", "Course List");
-		super.checkListingExists();
-		super.sortListing(0, "asc");
-		super.clickOnListingRecord(recordIndex);
-		super.checkFormExists();
+	//	@ParameterizedTest
+	//	@CsvFileSource(resources = "/lecturer/course/delete-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	//	public void positiveTest(final int recordIndex, final String code, final String title, final String resumen, final String retailPrice, final String link) {
+	//
+	//		super.signIn("lecturer1", "lecturer1");
+	//		super.clickOnMenu("Lecturer", "Course List");
+	//		super.checkListingExists();
+	//		super.sortListing(0, "asc");
+	//		super.clickOnListingRecord(recordIndex);
+	//		super.checkFormExists();
+	//
+	//		super.checkInputBoxHasValue("code", code);
+	//		super.checkInputBoxHasValue("title", title);
+	//		super.checkInputBoxHasValue("resumen", resumen);
+	//		super.checkInputBoxHasValue("retailPrice", retailPrice);
+	//		super.checkInputBoxHasValue("link", link);
+	//
+	//		super.clickOnSubmit("Delete Course");
+	//
+	//		super.signOut();
+	//	}
+	//
+	//	@ParameterizedTest
+	//	@CsvFileSource(resources = "/lecturer/course/delete-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	//	public void test200Negative(final int recordIndex, final String code, final String title, final String resumen, final String retailPrice, final String link) {
+	//		super.signIn("lecturer1", "lecturer1");
+	//		super.clickOnMenu("Lecturer", "Course List");
+	//		super.checkListingExists();
+	//		super.sortListing(0, "asc");
+	//		super.clickOnListingRecord(recordIndex);
+	//		super.checkFormExists();
+	//
+	//		super.checkInputBoxHasValue("code", code);
+	//		super.checkInputBoxHasValue("title", title);
+	//		super.checkInputBoxHasValue("resumen", resumen);
+	//		super.checkInputBoxHasValue("retailPrice", retailPrice);
+	//		super.checkInputBoxHasValue("link", link);
+	//
+	//		super.checkNotSubmitExists("Delete Course");
+	//
+	//		super.signOut();
+	//	}
 
-		super.checkInputBoxHasValue("code", code);
-		super.checkInputBoxHasValue("title", title);
-		super.checkInputBoxHasValue("resumen", resumen);
-		super.checkInputBoxHasValue("retailPrice", retailPrice);
-		super.checkInputBoxHasValue("link", link);
 
-		super.clickOnSubmit("Delete Course");
+	@Test
+	public void hackingTest() {
+		Collection<Course> courses;
+		String param;
 
-		super.signOut();
+		courses = this.repository.findManyCoursesByLecturerUsername("lecturer1");
+		for (final Course c : courses) {
+			param = String.format("id=%d", c.getId());
+
+			super.checkLinkExists("Sign in");
+			super.request("/lecturer/course/delete", param);
+			super.checkPanicExists();
+
+			super.signIn("administrator1", "administrator1");
+			super.request("/lecturer/course/delete", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("lecturer2", "lecturer2");
+			super.request("/lecturer/course/delete", param);
+			super.checkPanicExists();
+			super.signOut();
+
+		}
 	}
-
 }
