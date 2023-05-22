55c94d779391543177d2dc254d2c45324dbf5e02
albdomrui <albdomrui@alum.us.es>
2023-05-22 11:20:01 +0200

    Tests terminados

--- src/test/java/acme/testing/lecturer/course/LecturerCourseDeleteTest.java
+++ src/test/java/acme/testing/lecturer/course/LecturerCourseDeleteTest.java
@@ -4,6 +4,8 @@ package acme.testing.lecturer.course;
 import java.util.Collection;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import acme.entities.courses.Course;
@@ -14,49 +16,49 @@ public class LecturerCourseDeleteTest extends TestHarness {
 	@Autowired
 	protected LecturerCourseTestRepository repository;
 
-	//	@ParameterizedTest
-	//	@CsvFileSource(resources = "/lecturer/course/delete-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	//	public void positiveTest(final int recordIndex, final String code, final String title, final String resumen, final String retailPrice, final String link) {
-	//
-	//		super.signIn("lecturer1", "lecturer1");
-	//		super.clickOnMenu("Lecturer", "Course List");
-	//		super.checkListingExists();
-	//		super.sortListing(0, "asc");
-	//		super.clickOnListingRecord(recordIndex);
-	//		super.checkFormExists();
-	//
-	//		super.checkInputBoxHasValue("code", code);
-	//		super.checkInputBoxHasValue("title", title);
-	//		super.checkInputBoxHasValue("resumen", resumen);
-	//		super.checkInputBoxHasValue("retailPrice", retailPrice);
-	//		super.checkInputBoxHasValue("link", link);
-	//
-	//		super.clickOnSubmit("Delete Course");
-	//
-	//		super.signOut();
-	//	}
-	//
-	//	@ParameterizedTest
-	//	@CsvFileSource(resources = "/lecturer/course/delete-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-	//	public void test200Negative(final int recordIndex, final String code, final String title, final String resumen, final String retailPrice, final String link) {
-	//		super.signIn("lecturer1", "lecturer1");
-	//		super.clickOnMenu("Lecturer", "Course List");
-	//		super.checkListingExists();
-	//		super.sortListing(0, "asc");
-	//		super.clickOnListingRecord(recordIndex);
-	//		super.checkFormExists();
-	//
-	//		super.checkInputBoxHasValue("code", code);
-	//		super.checkInputBoxHasValue("title", title);
-	//		super.checkInputBoxHasValue("resumen", resumen);
-	//		super.checkInputBoxHasValue("retailPrice", retailPrice);
-	//		super.checkInputBoxHasValue("link", link);
-	//
-	//		super.checkNotSubmitExists("Delete Course");
-	//
-	//		super.signOut();
-	//	}
 
+	@ParameterizedTest
+	@CsvFileSource(resources = "/lecturer/course/delete-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void positiveTest(final int recordIndex, final String code, final String title, final String resumen, final String retailPrice, final String link) {
+
+		super.signIn("lecturer1", "lecturer1");
+		super.clickOnMenu("Lecturer", "Course List");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("resumen", resumen);
+		super.checkInputBoxHasValue("retailPrice", retailPrice);
+		super.checkInputBoxHasValue("link", link);
+
+		super.clickOnSubmit("Delete Course");
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/lecturer/course/delete-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200Negative(final int recordIndex, final String code, final String title, final String resumen, final String retailPrice, final String link) {
+		super.signIn("lecturer1", "lecturer1");
+		super.clickOnMenu("Lecturer", "Course List");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("resumen", resumen);
+		super.checkInputBoxHasValue("retailPrice", retailPrice);
+		super.checkInputBoxHasValue("link", link);
+
+		super.checkNotSubmitExists("Delete Course");
+
+		super.signOut();
+	}
 
 	@Test
 	public void hackingTest() {
--- src/test/java/acme/testing/lecturer/course/LecturerCoursePublishTest.java
+++ src/test/java/acme/testing/lecturer/course/LecturerCoursePublishTest.java
@@ -1,10 +1,14 @@
 
 package acme.testing.lecturer.course;
 
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.entities.courses.Course;
 import acme.testing.TestHarness;
 
 public class LecturerCoursePublishTest extends TestHarness {
@@ -15,7 +19,7 @@ public class LecturerCoursePublishTest extends TestHarness {
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/lecturer/course/publish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void positiveTest(final int recordIndex, final String title) {
+	public void positiveTest(final int recordIndex, final String title, final String lectures) {
 		super.signIn("lecturer1", "lecturer1");
 
 		super.clickOnMenu("Lecturer", "Course List");
@@ -25,87 +29,63 @@ public class LecturerCoursePublishTest extends TestHarness {
 
 		super.clickOnListingRecord(recordIndex);
 		super.checkFormExists();
-		super.clickOnSubmit("Publish");
+		super.clickOnButton("Add lecture");
+		super.checkFormExists();
+		super.fillInputBoxIn("lecture", lectures);
+		super.clickOnSubmit("Add lecture");
+		super.clickOnSubmit("Publish Course");
+		super.clickOnListingRecord(recordIndex);
+		super.checkNotSubmitExists("Publish course");
+
 		super.checkNotErrorsExist();
 
 		super.signOut();
 	}
 
-	//	@ParameterizedTest
-	//	@CsvFileSource(resources = "/company/practicum/publish-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-	//	public void negativeButtonTest(final int recordIndex, final String code) {
-	//		// HINT: this test attempts to publish a Practicum that cannot be published, yet.
-	//
-	//		super.signIn("company1", "company1");
-	//
-	//		super.clickOnMenu("Company", "Practicum list");
-	//		super.checkListingExists();
-	//		super.sortListing(0, "asc");
-	//
-	//		super.checkColumnHasValue(recordIndex, 0, code);
-	//		super.clickOnListingRecord(recordIndex);
-	//		super.checkFormExists();
-	//		super.checkNotSubmitExists("Publish");
-	//
-	//		super.signOut();
-	//	}
-	//
-	//	@Test
-	//	public void test300Hacking() {
-	//		// HINT: this test tries to publish a Practicum with a role other than "Employer".
-	//
-	//		final Collection<Practicum> practicums;
-	//		String params;
-	//
-	//		practicums = this.repository.findManyPracticumsByCompanyUsername("company1");
-	//		for (final Practicum practicum : practicums)
-	//			if (practicum.isDraftMode()) {
-	//				params = String.format("id=%d", practicum.getId());
-	//
-	//				super.checkLinkExists("Sign in");
-	//				super.request("/company/practicum/publish", params);
-	//				super.checkPanicExists();
-	//
-	//				super.signIn("administrator1", "administrator1");
-	//				super.request("/company/practicum/publish", params);
-	//				super.checkPanicExists();
-	//				super.signOut();
-	//
-	//			}
-	//	}
-	//
-	//	@Test
-	//	public void test301Hacking() {
-	//		// HINT: this test tries to publish a published Practicum that was registered by the principal.
-	//
-	//		Collection<Practicum> practicums;
-	//		String params;
-	//
-	//		super.signIn("company1", "company1");
-	//		practicums = this.repository.findManyPracticumsByCompanyUsername("company1");
-	//		for (final Practicum practicum : practicums)
-	//			if (!practicum.isDraftMode()) {
-	//				params = String.format("id=%d", practicum.getId());
-	//				super.request("/company/practicum/publish", params);
-	//			}
-	//		super.signOut();
-	//	}
-	//
-	//	@Test
-	//	public void test302Hacking() {
-	//		// HINT: this test tries to publish a Practicum that wasn't registered by the principal,
-	//		// HINT+ be it published or unpublished.
-	//
-	//		Collection<Practicum> practicums;
-	//		String params;
-	//
-	//		super.signIn("company2", "company2");
-	//		practicums = this.repository.findManyPracticumsByCompanyUsername("company1");
-	//		for (final Practicum practicum : practicums) {
-	//			params = String.format("id=%d", practicum.getId());
-	//			super.request("/company/practicum/publish", params);
-	//		}
-	//		super.signOut();
-	//	}
+	@ParameterizedTest
+	@CsvFileSource(resources = "/lecturer/course/publish-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void negativeButtonTest(final int recordIndex, final String title, final String lectures) {
+		// HINT: this test attempts to publish a Practicum that cannot be published, yet.
+		super.signIn("lecturer1", "lecturer1");
+
+		super.clickOnMenu("Lecturer", "Course List");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(recordIndex, 0, title);
+
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.checkNotSubmitExists("Publish course");
+
+		super.checkNotErrorsExist();
+
+		super.signOut();
+	}
+
+	@Test
+	public void hackingTest() {
+		Collection<Course> courses;
+		String param;
+
+		courses = this.repository.findManyCoursesByLecturerUsername("lecturer1");
+		for (final Course c : courses) {
+			param = String.format("id=%d", c.getId());
+
+			super.checkLinkExists("Sign in");
+			super.request("/lecturer/course/publish", param);
+			super.checkPanicExists();
+
+			super.signIn("administrator1", "administrator1");
+			super.request("/lecturer/course/publish", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("lecturer2", "lecturer2");
+			super.request("/lecturer/course/publish", param);
+			super.checkPanicExists();
+			super.signOut();
+
+		}
+	}
 
 }
