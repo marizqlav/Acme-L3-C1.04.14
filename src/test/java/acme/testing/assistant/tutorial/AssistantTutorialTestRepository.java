38043070ede63f3fd78b92ef9bc464a6741724b2
marolmmar1 <marolmmar1@alum.us.es>
2023-05-25 15:49:36 +0200

    Assistant tutorial show test successful

--- src/test/java/acme/testing/assistant/tutorial/AssistantTutorialTestRepository.java
+++ src/test/java/acme/testing/assistant/tutorial/AssistantTutorialTestRepository.java
@@ -1,6 +1,8 @@
+
 package acme.testing.assistant.tutorial;
 
+import acme.framework.repositories.AbstractRepository;
 
-public interface AssistantTutorialTestRepository {
+public interface AssistantTutorialTestRepository extends AbstractRepository {
 
 }
