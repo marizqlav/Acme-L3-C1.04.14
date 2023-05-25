79124a22de368ba09798b7528646cca88f838ae9
marolmmar1 <marolmmar1@alum.us.es>
2023-05-25 17:16:43 +0200

    tutorial publish tetsing done

--- src/test/java/acme/testing/assistant/tutorial/AssistantTutorialPublishTest.java
+++ src/test/java/acme/testing/assistant/tutorial/AssistantTutorialPublishTest.java
@@ -1,6 +1,47 @@
+
 package acme.testing.assistant.tutorial;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class AssistantTutorialPublishTest {
+import acme.testing.TestHarness;
 
+public class AssistantTutorialPublishTest extends TestHarness {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/assistant/tutorial/publish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void positiveTest(final int recordIndex, final String title, final String estimatedTime, final String description, final String course) {
+		super.signIn("assistant1", "assistant1");
+		super.clickOnMenu("Assistant", "Tutorial List");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(recordIndex, 0, title);
+		super.clickOnListingRecord(recordIndex);
+
+		super.clickOnSubmit("Publish");
+
+		super.checkColumnHasValue(recordIndex, 0, title);
+		super.clickOnListingRecord(recordIndex);
+
+		super.checkNotSubmitExists("Publish");
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/assistant/tutorial/publish-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void negativeTest(final int recordIndex, final String title, final String estimatedTime, final String description, final String course) {
+		super.signIn("assistant1", "assistant1");
+		super.clickOnMenu("Assistant", "Tutorial List");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(recordIndex, 0, title);
+		super.clickOnListingRecord(recordIndex);
+
+		super.checkNotSubmitExists("Publish");
+
+		super.signOut();
+	}
 }
