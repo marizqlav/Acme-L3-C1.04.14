191c4af5e9e73538f4272ef54a5d2d130c3098b3
marolmmar1 <marolmmar1@alum.us.es>
2023-05-25 16:24:24 +0200

    assistan tutorial create test seccessful

--- src/test/java/acme/testing/assistant/tutorial/AssistantTutorialCreateTest.java
+++ src/test/java/acme/testing/assistant/tutorial/AssistantTutorialCreateTest.java
@@ -1,6 +1,59 @@
+
 package acme.testing.assistant.tutorial;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class AssistantTutorialCreateTest {
+import acme.testing.TestHarness;
 
+public class AssistantTutorialCreateTest extends TestHarness {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/assistant/tutorial/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void positiveTest(final int recordIndex, final String title, final String estimatedTime, final String description, final String course) {
+
+		super.signIn("assistant1", "assistant1");
+		super.clickOnMenu("Assistant", "Tutorial List");
+
+		super.checkListingExists();
+		super.clickOnButton("Create");
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("description", description);
+		super.fillInputBoxIn("estimatedTime", estimatedTime);
+		super.fillInputBoxIn("course", course);
+		super.clickOnSubmit("Create");
+
+		super.clickOnMenu("Assistant", "Tutorial List");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(recordIndex, 0, title);
+
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("estimatedTime", estimatedTime);
+		super.checkInputBoxHasValue("course", course);
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/assistant/tutorial/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void negativeTest(final int recordIndex, final String title, final String estimatedTime, final String description, final String course) {
+
+		super.signIn("assistant1", "assistant1");
+		super.clickOnMenu("Assistant", "Tutorial List");
+
+		super.checkListingExists();
+		super.clickOnButton("Create");
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("description", description);
+		super.fillInputBoxIn("estimatedTime", estimatedTime);
+		super.fillInputBoxIn("course", course);
+		super.clickOnSubmit("Create");
+
+		super.checkErrorsExist();
+		super.signOut();
+	}
 }
