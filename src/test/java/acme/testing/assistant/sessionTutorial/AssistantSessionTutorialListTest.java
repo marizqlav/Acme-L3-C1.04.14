856e4cfe6ae4253959b9902b1160497906067401
marolmmar1 <marolmmar1@alum.us.es>
2023-05-26 13:43:29 +0200

    session tutorial list test succesful

--- src/test/java/acme/testing/assistant/sessionTutorial/AssistantSessionTutorialListTest.java
+++ src/test/java/acme/testing/assistant/sessionTutorial/AssistantSessionTutorialListTest.java
@@ -1,6 +1,31 @@
+
 package acme.testing.assistant.sessionTutorial;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class AssistantSessionTutorialListTest {
+import acme.testing.TestHarness;
 
+public class AssistantSessionTutorialListTest extends TestHarness {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/assistant/sessionTutorial/list.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int tutorial, final int indexRecord, final String title, final String description) {
+
+		super.signIn("assistant1", "assistant1");
+		super.clickOnMenu("Assistant", "Tutorial List");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.clickOnListingRecord(tutorial);
+
+		super.clickOnButton("Sessions");
+
+		super.checkListingExists();
+
+		super.checkColumnHasValue(indexRecord, 0, title);
+		super.checkColumnHasValue(indexRecord, 1, description);
+
+		super.signOut();
+	}
 }
