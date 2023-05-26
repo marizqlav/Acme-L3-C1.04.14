1039ec5923e0804d9ae43b5d5b66c451fdd51aae
marolmmar1 <marolmmar1@alum.us.es>
2023-05-26 12:25:51 +0200

    delete tutorial test sucess

--- src/test/java/acme/testing/assistant/tutorial/AssistantTutorialDeleteTest.java
+++ src/test/java/acme/testing/assistant/tutorial/AssistantTutorialDeleteTest.java
@@ -1,6 +1,46 @@
+
 package acme.testing.assistant.tutorial;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class AssistantTutorialDeleteTest {
+import acme.testing.TestHarness;
+
+public class AssistantTutorialDeleteTest extends TestHarness {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/assistant/tutorial/delete-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200Positive(final int recordIndex, final String title, final String estimatedTime, final String description, final String course) {
+
+		super.signIn("assistant1", "assistant1");
+		super.clickOnMenu("Assistant", "Tutorial List");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.clickOnListingRecord(recordIndex);
+
+		super.checkFormExists();
+
+		super.clickOnSubmit("Delete");
+
+		super.checkListingExists();
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/assistant/tutorial/delete-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void negative100Test(final int recordIndex, final String title, final String estimatedTime, final String description, final String course) {
+		super.signIn("assistant1", "assistant1");
+		super.clickOnMenu("Assistant", "Tutorial List");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(recordIndex, 0, title);
+		super.clickOnListingRecord(recordIndex);
+
+		super.checkNotSubmitExists("Delete");
+
+		super.signOut();
+	}
 
 }
