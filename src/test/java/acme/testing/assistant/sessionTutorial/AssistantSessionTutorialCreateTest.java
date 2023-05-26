bdfe71c0ecc19837e7c9db1ecc79bb4796e7a8a3
marolmmar1 <marolmmar1@alum.us.es>
2023-05-26 16:31:32 +0200

    session tutorial create tests

--- src/test/java/acme/testing/assistant/sessionTutorial/AssistantSessionTutorialCreateTest.java
+++ src/test/java/acme/testing/assistant/sessionTutorial/AssistantSessionTutorialCreateTest.java
@@ -1,6 +1,75 @@
+
 package acme.testing.assistant.sessionTutorial;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class AssistantSessionTutorialCreateTest {
+import acme.testing.TestHarness;
 
+public class AssistantSessionTutorialCreateTest extends TestHarness {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/assistant/sessionTutorial/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int tutorial, final int indexREcord, final String title, final String description, final String type, final String startDate, final String endDate, final String link) {
+
+		super.signIn("assistant1", "assistant1");
+		super.clickOnMenu("Assistant", "Tutorial List");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.clickOnListingRecord(tutorial);
+
+		super.clickOnButton("Sessions");
+
+		super.clickOnButton("Create");
+
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("description", description);
+		super.fillInputBoxIn("sessionType", type);
+		super.fillInputBoxIn("startDate", startDate);
+		super.fillInputBoxIn("endDate", endDate);
+		super.fillInputBoxIn("link", link);
+
+		super.clickOnSubmit("Create");
+
+		super.checkListingExists();
+
+		super.checkColumnHasValue(indexREcord, 0, title);
+		super.clickOnListingRecord(indexREcord);
+
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("sessionType", type);
+		super.checkInputBoxHasValue("startDate", startDate);
+		super.checkInputBoxHasValue("endDate", endDate);
+		super.checkInputBoxHasValue("link", link);
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/assistant/sessionTutorial/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200Negative(final int tutorial, final int indexREcord, final String title, final String description, final String type, final String startDate, final String endDate, final String link) {
+
+		super.signIn("assistant1", "assistant1");
+		super.clickOnMenu("Assistant", "Tutorial List");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.clickOnListingRecord(tutorial);
+
+		super.clickOnButton("Sessions");
+
+		super.clickOnButton("Create");
+
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("description", description);
+		super.fillInputBoxIn("sessionType", type);
+		super.fillInputBoxIn("startDate", startDate);
+		super.fillInputBoxIn("endDate", endDate);
+		super.fillInputBoxIn("link", link);
+
+		super.clickOnSubmit("Create");
+
+		super.checkErrorsExist();
+	}
 }
