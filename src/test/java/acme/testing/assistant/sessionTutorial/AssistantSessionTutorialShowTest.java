a72520fba87a2498867b716cf6241b4f1e9b4b71
marolmmar1 <marolmmar1@alum.us.es>
2023-05-26 13:12:13 +0200

    session tutorial show test

--- src/test/java/acme/testing/assistant/sessionTutorial/AssistantSessionTutorialShowTest.java
+++ src/test/java/acme/testing/assistant/sessionTutorial/AssistantSessionTutorialShowTest.java
@@ -1,6 +1,38 @@
+
 package acme.testing.assistant.sessionTutorial;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class AssistantSessionTutorialShowTest {
+import acme.testing.TestHarness;
+
+public class AssistantSessionTutorialShowTest extends TestHarness {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/assistant/sessionTutorial/show.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int tutorial, final int indexREcord, final String title, final String description, final String type, final String startDate, final String endDate, final String link) {
+
+		super.signIn("assistant1", "assistant1");
+		super.clickOnMenu("Assistant", "Tutorial List");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.clickOnListingRecord(tutorial);
+
+		super.clickOnButton("Sessions");
+
+		super.checkListingExists();
+
+		super.checkColumnHasValue(indexREcord, 0, title);
+		super.clickOnListingRecord(indexREcord);
+
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("sessionType", type);
+		super.checkInputBoxHasValue("startDate", startDate);
+		super.checkInputBoxHasValue("endDate", endDate);
+		super.checkInputBoxHasValue("link", link);
+		super.signOut();
+	}
 
 }
